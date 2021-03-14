43c34544abe980604329dded4028d0f28cfbf174
NadineTi <kayahetalia@gmail.com>
2021-03-14 12:31:52 +0100

    360° Aquarium
    Aquarium Glas bearbeitet
    360° Aquarium erste Wände eingefügt
    Erster Fisch hinter dem Glas, der sich bewegt

---
+++ Assets/Bewegung Fische/Fische.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fische : MonoBehaviour {
+
+    public LevelManager levelManager;
+    public Vector3 startPos;
+    public Vector3 newPos;
+
+
+
+    // Start is called before the first frame update
+    void Start(){
+        levelManager = FindObjectOfType<LevelManager>();
+        startPos = transform.position;
+        newPos = startPos;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPos.z = newPos.z + Mathf.PingPong(Time.time, 6) - 3;
+        transform.position = newPos;
+
+    }
+    
+        }
