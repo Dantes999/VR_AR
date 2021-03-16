b0c8fdb926745a4a37d7d7728d49df9b9b6801b8
Arthur <Arthur@Test.de>
2021-03-16 10:13:26 +0100

    Camera2 WASD MOVE

---
+++ Assets/moveTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public int Speed = 1;
+
+    void Update()
+    {
+        float xAxisValue = (Input.GetAxis("Horizontal") * Speed) / 50;
+        float zAxisValue = (Input.GetAxis("Vertical") * Speed) / 50;
+
+        transform.position = new Vector3(transform.position.x + xAxisValue, transform.position.y, transform.position.z + zAxisValue);
+    }
+}
