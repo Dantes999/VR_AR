d2755dd02954596728c127f919e5e666abc30073
NadineTi <kayahetalia@gmail.com>
2021-03-18 11:26:19 +0100

    Fisch
    Bewegungstest die 2te

--- Assets/Bewegung Fische/Fischex.cs
+++ Assets/Bewegung Fische/Fischex.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Fischex : MonoBehaviour {
+public class Fischex : MonoBehaviour
+{
 
-   
+
     public GameObject Fische;
     bool buttonPressed;
 
@@ -14,29 +15,28 @@ public class Fischex : MonoBehaviour {
 
 
     // Start is called before the first frame update
-    void Start(){
-       
+    void Start()
+    {
+
         pos2 = new Vector3(11, 1, 11);
         //rot2 = new Vector3(-90, 90, 0);
-        //buttonPressed = false;
+        buttonPressed = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown("f"))
-        //{
-          //  buttonPressed = true;
-        //}
-        //if (buttonPressed == true)
-        //{
+        if (Input.GetKeyDown("f"))
+        {
+            buttonPressed = true;
+        }
+        if (buttonPressed == true)
+        {
             Fische.transform.position = Vector3.Lerp(Fische.transform.position, pos2, 3 * Time.deltaTime);
             //Fische.transform.rotation = Quaternion.Lerp(Fische.transform.position, Quaternion.Euler(rot2), 3 * Time.deltaTime); 
-        //}
-
-
+        }
 
     }
-    
-        }
+
+}
